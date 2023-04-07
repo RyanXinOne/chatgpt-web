12cb8ffbb949c75d434be9ff12ce6b8fe50537c9
RyanXin <xyz@ryanxin.cn>
2023-04-07 11:57:33 +0100

    fix: old regression update

--- src/store/modules/auth/helper.ts
+++ src/store/modules/auth/helper.ts
@@ -3,12 +3,6 @@ import { ss } from '@/utils/storage'
 const LOCAL_NAME = 'SECRET_TOKEN'
 
 export function getToken() {
-  // replace old 'SECRET_KEY' with 'SECRET_TOKEN' if exists
-  const oldToken = ss.get('SECRET_KEY')
-  if (oldToken) {
-    ss.set(LOCAL_NAME, oldToken)
-    ss.remove('SECRET_KEY')
-  }
   return ss.get(LOCAL_NAME)
 }
 
