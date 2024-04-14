27a44a5e5a3f7ebad4936a7dc7312362c7ea3c05
Ryan Xin <xyz@ryanxin.cn>
2024-04-14 22:35:10 +0800

    feat: update to endpoint gpt-4-turbo-2024-04-09

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -64,7 +64,7 @@ let api3: ChatGPTAPI, api4: ChatGPTAPI
 
   api4 = new ChatGPTAPI({
     ...options,
-    completionParams: { model: 'gpt-4-0125-preview' },
+    completionParams: { model: 'gpt-4-turbo-2024-04-09' },
     maxModelTokens: 32000,
     maxResponseTokens: 4000,
   })
